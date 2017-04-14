e0213018e33fca45c35188e0fcb4a9f8be2cf687
Leonardo Bighetti <leo@invitedhome.com>
2017-04-14 16:51:24 -0600

    basic test for it

--- test/clark_kent_test.rb
+++ test/clark_kent_test.rb
@@ -4,4 +4,9 @@ class ClarkKentTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, ClarkKent
   end
+
+  test ".bucket_name is configurable" do
+    ClarkKent.config(:bucket_name => "trololo")
+    assert_match "trololo", ClarkKent.bucket_name
+  end
 end
