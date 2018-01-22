dd6be00123e9995b63d2c55a06e62353776c89e4
Eric Draut <edraut@gmail.com>
2018-01-22 13:05:09 -0700

    oversight

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def prepare_filters
-    fc_struct = Struct.new(:orders,:departments,:users)
-    @filter_collections = fc_struct.new(::Order.all, ::Department.all, ::User.all)
-  end
 
   def current_user
 
